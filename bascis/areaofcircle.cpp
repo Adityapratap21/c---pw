ee93ec9cf59e681f0c2670508e9ea186735a9403
Adityapratap21 <138037272+Adityapratap21@users.noreply.github.com>
2024-05-30 20:11:08 +0530

    Update areaofcircle.cpp

--- bascis/areaofcircle.cpp
+++ bascis/areaofcircle.cpp
@@ -1,9 +1,20 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-        int main(){
-          float r=8;
-          float a;
-          a=3.1415*r*r;
-          cout<<a;
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<int>v(n); 
+        for(int i=0;i<n;i++) cin>>v[i];
 
+        for(int i=1;i<n;i++){
+            if(v[i]%v[i-1]==0){
+                v[i-1]++;
+                if(i>1 && v[i-1]==v[i-2]) v[i-1]++;
+                 if(v[i]==v[i-1]) v[i-1]++;
+            }
         }
+        for(int i=0;i<n;i++) cout<<v[i]<<" ";
+        cout<<endl;
+    }
+}
