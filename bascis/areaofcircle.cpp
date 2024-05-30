c80c6a79c670bd635639158422caa5ade99678e0
Adityapratap21 <138037272+Adityapratap21@users.noreply.github.com>
2024-05-30 20:43:36 +0530

    Update areaofcircle.cpp

--- bascis/areaofcircle.cpp
+++ bascis/areaofcircle.cpp
@@ -7,7 +7,7 @@ int main(){
         vector<int>v(n); 
         for(int i=0;i<n;i++) cin>>v[i];
 
-          for(int i=1;i<n;i++){
+          for(int i=0;i<n;i++){
             if(v[i]==1) v[i]++;
         }
 
